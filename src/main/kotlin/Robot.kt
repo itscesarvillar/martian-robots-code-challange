8983f6ef346a28b6bb668ce3eec34d70d3cd1d52
César Villar <cesar.villar@kairosds.com>
2021-04-13 12:05:18 +0200

    feat: robot moves

--- src/main/kotlin/Robot.kt
+++ src/main/kotlin/Robot.kt
@@ -1,8 +1,43 @@
 class Robot (private val radio: Radio, spawnPosition: Position) {
-  val position = spawnPosition
+  var position = spawnPosition
 
-  fun execute () {
+  fun execute (instruction: Instruction) {
     radio.uplink(this.position)
-    // then moves
+    when (instruction) {
+      Instruction.R -> this.moveRight()
+      Instruction.L -> this.moveLeft()
+      Instruction.F -> this.moveForward()
+    }
+  }
+
+  private fun moveRight() {
+    val newOrientation = when (this.position.orientation) {
+      Orientation.N -> Orientation.E
+      Orientation.S -> Orientation.W
+      Orientation.E -> Orientation.S
+      Orientation.W -> Orientation.N
+    }
+    this.position = Position(this.position.point, newOrientation)
+  }
+
+  private fun moveLeft() {
+    val newOrientation = when (this.position.orientation) {
+      Orientation.N -> Orientation.W
+      Orientation.S -> Orientation.E
+      Orientation.E -> Orientation.N
+      Orientation.W -> Orientation.S
+    }
+    this.position = Position(this.position.point, newOrientation)
+  }
+
+  private fun moveForward() {
+    val delta = when (this.position.orientation) {
+      Orientation.N -> Pair(0,1)
+      Orientation.S -> Pair(0,-1)
+      Orientation.E -> Pair(1,0)
+      Orientation.W -> Pair(-1,0)
+    }
+    val (x,y) = this.position.point
+    this.position = Position(Point(x + delta.first, y + delta.second), this.position.orientation)
   }
 }
